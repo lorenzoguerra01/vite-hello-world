d4538034472b49be97fff6c4ea07a889c1b2ea0a
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-09 17:38:58 +0200

    scaffolding + title from appTitle component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppTitle />
+</template>
+
+<script>
+import AppTitle from '@/components/AppTitle.vue'
+export default {
+  name: 'App',
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped></style>
